6ecc28575e6056e9cf3752704e1d8c8fc82b412e
yeongtaek, Hong <mozodev@users.noreply.github.com>
2018-07-31 11:57:11 +0900

    동영상 추가를 위해 ckeditor embed 플러그인을 추가합니다.

--- docroot/common/js/plugins/ckeditor/ckeditor/config.js
+++ docroot/common/js/plugins/ckeditor/ckeditor/config.js
@@ -1,3 +1,4 @@
 CKEDITOR.editorConfig = function( config ) {
-	config.extraPlugins = 'clipboard,pastetext,pastefromword,notification';
+	config.extraPlugins = 'clipboard,pastetext,pastefromword,notification,notificationaggregator,widgetselection,widget,lineutils,embedbase,embed';
+	config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}';
 };
